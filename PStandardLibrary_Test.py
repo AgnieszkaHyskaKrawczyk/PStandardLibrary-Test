3767a6862f7d6b890e6e1ae9a89097588b9389cf
Agnieszka <agnieszka.krawczyk@seznam.cz>
2019-05-26 19:21:35 +0200

    all30

--- PStandardLibrary_Test.py
+++ PStandardLibrary_Test.py
@@ -1,2 +1,28 @@
 print('this program contains all funtion from standard python library')
-print('hello')
+x = 3
+y = 4
+z = 3 + 4
+print(str(z))
+x = 13
+y = 5
+z = x//y
+q = x%y
+print(str(z))
+print(str(q))
+x = 10
+y = -2
+print(str(abs(x)))
+print(str(abs(y)))
+x = 13
+y = 5
+print(str(divmod(x, y)))
+print(str(pow(x, y)))
+print(str(x ** y))
+print(str(x | y))
+print(str(x ^ y))
+print(str(x & y))
+print(str(x << 2))
+print(str(x >> 2))
+print(str(x))
+print(bin(x))
+print(bin(y))
